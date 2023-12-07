4303df0ffc01bd3cc45d93e1b420affbd3dc1818
Sérgio Gabriel <sgabriel.cafe@gmail.com>
2023-12-07 19:04:22 -0300

    feat: creation of Services and models for loginPage

---
+++ XamarinFirebaseCRUD/Service/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XamarinFirebaseCRUD.Service
+{
+    public interface IUserService
+    {
+        Task<bool> RegisterUser(string username, string password);
+        Task<bool> UserExist(string login, string loginPassword);
+
+    }
+}
---
+++ XamarinFirebaseCRUD/Service/UserService.cs
@@ -0,0 +1,54 @@
+﻿using Firebase.Database;
+using Firebase.Database.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using XamarinFirebaseCRUD.Models;
+
+namespace XamarinFirebaseCRUD.Service
+{
+    public class UserService : IUserService
+    {
+        protected static string PasswordFirebase = "JQewHX09K0fjaEnxxoAeEtaxWvhTV40dgUEVQEzH";
+        FirebaseClient Client = new FirebaseClient("https://agendaapp-aace7-default-rtdb.firebaseio.com/",
+            new FirebaseOptions { AuthTokenAsyncFactory = () => Task.FromResult(PasswordFirebase)});
+
+        public async Task<bool> RegisterUser(string username, string password)
+        {
+            try
+            {
+                await Client.Child("Users")
+                    .PostAsync(new Users()
+                    {
+                        User = username,
+                        Password = password
+                    });
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public async Task<bool> UserExist(string login, string loginPassword)
+        {
+            try
+            {
+                var consult = (await Client.Child("Users")
+                    .OnceAsync<Users>())
+                    .Where(u => u.Object.User == login & u.Object.Password == loginPassword)
+                    .FirstOrDefault();
+                return consult != null;
+            }
+            catch (Exception ex)
+            {
+                await Shell.Current.DisplayAlert("Usuario já criado", "O usuário existe, por favor, utilize outro email ou acesse o email criado.", "Ok");
+                return false;
+            }
+        }
+    }
+}
